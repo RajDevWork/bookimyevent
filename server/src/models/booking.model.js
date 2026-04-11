4dabb4f76386383c2ed25ca09b4ddb2ec55c10ff
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-04-11 17:48:25 +0530

    Added booking model schema

--- server/src/models/booking.model.js
+++ server/src/models/booking.model.js
@@ -1,7 +1,36 @@
 const mongoose = require("mongoose")
 
 const bookingSchema = new mongoose.Schema({
-        
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'users',
+            required:[true,'User id is required']
+        },
+        eventId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'events',
+            required:[true,'Event id is required']
+        },
+        numberOfTickets:{
+            type:Number,
+            required:[true,'Number of tickets is required']
+        },
+        status:{
+            type:String,
+            enum:["pending","confirmed","cancelled"],
+            default:"pending"
+        },
+        paymentStatus:{
+            type:String,
+            enum:["non_paid","paid"],
+            default:"non_paid"
+        },
+        amount:{
+            type:Number,
+            required:[true,'Amount is required']
+        }
+},{
+    timestamps:true
 })
 
 const bookingModel = mongoose.model('bookings',bookingSchema)
