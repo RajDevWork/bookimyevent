ff9f742d3ffbf5621a5384d3cabcba051fa1d316
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-04-14 16:02:15 +0530

    Added event related pages for user

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
-
+const cors = require("cors")
 const authRouter = require("./routes/auth.routes")
 const eventRouter = require("./routes/event.routes")
 const bookingRouter = require("./routes/booking.routes")
@@ -9,6 +9,10 @@ app = express() // create server
 
 app.use(cookieParser())
 app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/auth",authRouter)
 app.use("/api/events",eventRouter)
